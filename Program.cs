5c0b4ad363d18a1fd82c5bb9e68040610fbc1ced
Yusak Abiyyu <yusakabiyyu@gmail.com>
2026-03-28 08:26:24 +0700

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Modul4_103022400040;
+using System;
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        KodePaket kp = new KodePaket();
+
+        Console.WriteLine("Kode paket Basic: " + kp.GetKodePaket("Basic"));
+        Console.WriteLine("Kode paket Premium: " + kp.GetKodePaket("Premium"));
+        Console.WriteLine("Kode paket Student: " + kp.GetKodePaket("Student"));
+        Console.WriteLine();
+
+    }
+}
